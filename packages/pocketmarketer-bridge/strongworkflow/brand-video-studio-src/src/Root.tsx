6ac3b778ba14fae693ee67488fb1861e6dd93b1e
urbanplanningpros <info@urbanplanningpros.com>
2026-09-23 20:19:46 -0500

    Register dynamic BrandVideo Remotion composition

---
+++ packages/pocketmarketer-bridge/strongworkflow/brand-video-studio-src/src/Root.tsx
@@ -0,0 +1,27 @@
+import {Composition} from 'remotion';
+import {BrandVideo} from './BrandVideo';
+import {sampleJob} from './sample-job';
+import type {BrandVideoJob} from './types';
+import {normalizeJob, toDurationInFrames} from './types';
+
+export const RemotionRoot = () => (
+  <Composition<BrandVideoJob>
+    id="BrandVideo"
+    component={BrandVideo}
+    defaultProps={sampleJob}
+    durationInFrames={toDurationInFrames(sampleJob)}
+    fps={sampleJob.output.fps}
+    width={sampleJob.output.width}
+    height={sampleJob.output.height}
+    calculateMetadata={({props}) => {
+      const job = normalizeJob(props);
+      return {
+        durationInFrames: toDurationInFrames(job),
+        fps: job.output.fps,
+        width: job.output.width,
+        height: job.output.height,
+        props: job,
+      };
+    }}
+  />
+);
